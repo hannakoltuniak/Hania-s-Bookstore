d9241dbd970fa5708a45e1090907f6c5a8f516de
Hania Kołtuniak <hanna.koltuniak@gmail.com>
2024-03-20 15:18:20 +0100

    added accessing posted data

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -14,8 +14,34 @@ namespace HaniasBookstore.Controllers
             this.cart = cart;
         }
 
-        public IActionResult Checkout()
+        public IActionResult Checkout() //GET
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = cart.GetShoppingCartItems();
+            cart.ShoppingCartItems = items;
+
+            if (cart.ShoppingCartItems.Count == 0)
+                ModelState.AddModelError("", "Your cart is empty, add some books first");
+
+            if (ModelState.IsValid)
+            {
+                this.order.CreateOrder(order);
+                cart.ClearCart();
+
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thank you for your order! You'll soon read amazing stories!";
             return View();
         }
     }
